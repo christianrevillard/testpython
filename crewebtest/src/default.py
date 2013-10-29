d2a5cc66729d67ac22ae77647238b2cf42e368e4
christianrevillard <christian.revillard@centraliens.net>
2013-10-29 22:38:46 +0100

    this is my message

---
+++ crewebtest/src/default.py
@@ -0,0 +1,17 @@
+import webapp2
+import jinja2
+import os
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        
+        template_values = {}
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([('/', MainPage),('/page1', MainPage)], debug=True)
