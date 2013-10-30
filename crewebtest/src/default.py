50b4bda72099bde04d691798ea757136606d2f6b
christianrevillard <christian.revillard@centraliens.net>
2013-10-30 22:53:22 +0100

    test

--- crewebtest/src/default.py
+++ crewebtest/src/default.py
@@ -1,6 +1,10 @@
 import webapp2
 import jinja2
 import os
+import TestForms.testForms
+import HelloWorld.helloworld
+import StaticFiles.template
+import Templates.template
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -14,4 +18,13 @@ class MainPage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
 
-app = webapp2.WSGIApplication([('/', MainPage),('/page1', MainPage)], debug=True)
+app = webapp2.WSGIApplication([
+        ('/', MainPage),
+        ('/testForms', TestForms.testForms.TestForms),
+        ('/testForms/sign', TestForms.testForms.Guestbook),
+        ('/helloWorld', HelloWorld.helloworld.HelloWorld),
+        ('/staticFiles', StaticFiles.template.MainPage),
+        ('/staticFiles/sign', StaticFiles.template.Guestbook),
+        ('/templates', Templates.template.MainPage),
+        ('/templates/sign', Templates.template.Guestbook)
+        ], debug=True)
